01e628bae69c8758e84d5e0f872557055b46ee80
Jonathan Stark <jstark@jonathanstark.com>
2014-04-22 11:03:16 -0400

    removed jquery dependency

---
+++ 5. Remove jQuery Dependency/modernizr-server-plus.php
@@ -0,0 +1,58 @@
+<?php
+    if (isset($_COOKIE['MODERNIZR'])) {
+        $MODERNIZR = $_COOKIE['MODERNIZR'];
+        return;
+    }
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Detecting browser features...</title>
+        <script>
+
+            <?php include 'js/modernizr.js' ?>
+
+            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+            var $ = {
+                cookie: function(name, value, options) {
+                    var expires = options.expires || 0;
+                    var date = new Date();
+                    date.setDate(date.getDate() + options.expires);
+                    document.cookie = name + '=' + value + '; expires=' + date.toString();
+                }
+            }
+
+        </script>
+    </head>
+    <body>
+        <noscript>
+            <p>Sorry! This site requires javascript :(</p>
+        </noscript>
+        <script>
+            // Delete unused properties to minimize cookie size
+            delete Modernizr._cssomPrefixes;
+            delete Modernizr._domPrefixes;
+            delete Modernizr._prefixes;
+            delete Modernizr._version;
+            // Add extras
+            navigator.standalone = navigator.standalone || (screen.height-document.documentElement.clientHeight<10); // Polyfill for nav.standalone
+            var extras = {
+                devicepixelratio: window.devicePixelRatio,
+                screenheight: screen.height,
+                screenwidth: screen.width,
+                windowheight: window.innerHeight,
+                windowwidth: window.innerWidth,
+                standalone: window.navigator.standalone
+            }
+            Modernizr.extras = extras;
+            $.cookie("MODERNIZR", JSON.stringify(Modernizr), {expires: 7});
+            if (navigator.cookieEnabled) {
+                location.reload(true);
+            } else {
+                document.write('<p>Sorry! This site requires cookies :(</p>');
+            }
+        </script>
+    </body>
+</html>
+<?php die; // IMPORTANT: this line prevents execution from returning to the parent script in cases where cookies are disabled ?>
